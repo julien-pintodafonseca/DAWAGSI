c7b06bc11968866044eaa5c247dd5873c0fe1301
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-06 20:48:55 +0200

    Edit app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { DataService } from './services/data.service';
+
+import { FormsModule } from '@angular/Forms';
+import { ImageUploadModule } from 'angular2-image-upload';
+import { AppRoutingModule } from './app-routing.module';
+import { LibraryComponent } from './components/library/library.component';
+import { UploadImageComponent } from './components/upload-image/upload-image.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
+  /* composants */
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LibraryComponent,
+    UploadImageComponent,
+    PageNotFoundComponent
   ],
+  /* modules */
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    ImageUploadModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  /* services */
+  providers: [
+    DataService
+  ],
+  /* autre */
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
