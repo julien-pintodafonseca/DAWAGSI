ae466df7f11b1662324d7567a68d5133e1547442
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-06 20:48:34 +0200

    Add upload-image component

---
+++ src/app/components/upload-image/upload-image.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-image',
+  templateUrl: './upload-image.component.html',
+  styleUrls: ['./upload-image.component.css']
+})
+
+export class UploadImageComponent implements OnInit {
+
+  customStyle: any;
+
+  constructor() {
+    this.customStyle = {
+      selectButton: {
+        "color": "white",
+        "background-color": "orange",
+      },
+      clearButton: {
+        "display": "none"
+      },
+      layout: {
+        "width": "100%"
+      },
+      previewPanel: {
+        "background-color": "#FFF3E8",
+      }
+    };
+  }
+
+  ngOnInit() {
+
+  }
+
+}
+
