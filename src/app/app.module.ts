e285cffa92188e12a7db5805b54eafa5c7e376f6
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-10-07 02:11:10 +0200

    Add ngx-smart-modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxSmartModalModule } from 'ngx-smart-modal';
+import { ImageUploadModule } from 'angular2-image-upload';
+
+import { DataService } from './services/data.service';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
-import { DataService } from './services/data.service';
-
-import { FormsModule } from '@angular/Forms';
-import { ImageUploadModule } from 'angular2-image-upload';
-import { AppRoutingModule } from './app-routing.module';
 import { LibraryComponent } from './components/library/library.component';
 import { UploadImageComponent } from './components/upload-image/upload-image.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -18,14 +19,15 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     AppComponent,
     UserComponent,
     LibraryComponent,
-    UploadImageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    UploadImageComponent
   ],
   /* modules */
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
+    NgxSmartModalModule.forRoot(),
     ImageUploadModule.forRoot()
   ],
   /* services */
--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-library',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class LibraryComponent implements OnInit {
 
-  constructor() {
+  constructor(public ngxSmartModalService: NgxSmartModalService) {
 
   }
 
