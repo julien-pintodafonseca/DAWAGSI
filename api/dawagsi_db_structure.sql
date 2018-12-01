cfc9b975bf38eddcdd9aca61ee8d5654d86d212c
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-12-01 20:35:13 +0100

    Optimisation de la structure bdd relationnelle

--- api/dawagsi_db_structure.sql
+++ api/dawagsi_db_structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  sam. 01 déc. 2018 à 20:06
+-- Généré le :  sam. 01 déc. 2018 à 20:32
 -- Version du serveur :  5.6.41-log
 -- Version de PHP :  7.2.7
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Annotation` (
   `id` int(11) NOT NULL COMMENT 'ID de l''annotation',
+  `image` int(11) NOT NULL COMMENT 'ID de l''image de l''annotation',
   `tag` varchar(100) NOT NULL COMMENT 'Tag de l''annotation',
   `position` text NOT NULL COMMENT 'Données de position de l''annotation'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -55,31 +56,8 @@ CREATE TABLE `Image` (
   `id` int(11) NOT NULL COMMENT 'ID de l''image',
   `originalName` varchar(100) NOT NULL COMMENT 'Nom original de l''image',
   `generatedName` varchar(100) NOT NULL COMMENT 'Nom md5 de l''image',
+  `list` int(11) NOT NULL COMMENT 'ID de la liste de l''image',
   `editor` int(11) DEFAULT NULL COMMENT 'ID de l''éditeur lié à l''image'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Image_Annotation`
---
-
-CREATE TABLE `Image_Annotation` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre Image et Annotation',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image',
-  `annotation` int(11) NOT NULL COMMENT 'ID de l''annotation'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Image_Relation`
---
-
-CREATE TABLE `Image_Relation` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre Image et Relation',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image',
-  `relation` int(11) NOT NULL COMMENT 'ID de la relation'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -97,23 +75,12 @@ CREATE TABLE `List` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `List_Image`
---
-
-CREATE TABLE `List_Image` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre List et Image',
-  `list` int(11) NOT NULL COMMENT 'ID de la liste',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `Relation`
 --
 
 CREATE TABLE `Relation` (
   `id` int(11) NOT NULL COMMENT 'ID de la relation',
+  `image` int(11) NOT NULL COMMENT 'ID de l''image de la relation',
   `predicate` varchar(100) NOT NULL COMMENT 'Prédicat de relation',
   `annotation1` int(11) NOT NULL COMMENT 'ID de la première annotation de la relation',
   `annotation2` int(11) NOT NULL COMMENT 'ID de la seconde annotation de la relation'
@@ -127,7 +94,8 @@ CREATE TABLE `Relation` (
 -- Index pour la table `Annotation`
 --
 ALTER TABLE `Annotation`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Annotation_ibfk_1` (`image`);
 
 --
 -- Index pour la table `Editor`
@@ -140,23 +108,8 @@ ALTER TABLE `Editor`
 --
 ALTER TABLE `Image`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `Image_ibfk_1` (`editor`);
-
---
--- Index pour la table `Image_Annotation`
---
-ALTER TABLE `Image_Annotation`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `image` (`image`,`annotation`),
-  ADD KEY `Image_Annotation_ibfk_2` (`annotation`);
-
---
--- Index pour la table `Image_Relation`
---
-ALTER TABLE `Image_Relation`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `image` (`image`,`relation`),
-  ADD KEY `Image_Relation_ibfk_2` (`relation`);
+  ADD KEY `Image_ibfk_1` (`list`),
+  ADD KEY `Image_ibfk_2` (`editor`);
 
 --
 -- Index pour la table `List`
@@ -165,20 +118,13 @@ ALTER TABLE `List`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `List_Image`
---
-ALTER TABLE `List_Image`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `list` (`list`,`image`),
-  ADD KEY `List_Image_ibfk_2` (`image`);
-
---
 -- Index pour la table `Relation`
 --
 ALTER TABLE `Relation`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `Relation_ibfk_1` (`annotation1`),
-  ADD KEY `Relation_ibfk_2` (`annotation2`);
+  ADD KEY `Relation_ibfk_1` (`image`),
+  ADD KEY `Relation_ibfk_2` (`annotation1`),
+  ADD KEY `Relation_ibfk_3` (`annotation2`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -219,38 +165,25 @@ ALTER TABLE `Relation`
 --
 
 --
+-- Contraintes pour la table `Annotation`
+--
+ALTER TABLE `Annotation`
+  ADD CONSTRAINT `Annotation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Contraintes pour la table `Image`
 --
 ALTER TABLE `Image`
-  ADD CONSTRAINT `Image_ibfk_1` FOREIGN KEY (`editor`) REFERENCES `Editor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Image_Annotation`
---
-ALTER TABLE `Image_Annotation`
-  ADD CONSTRAINT `Image_Annotation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Image_Annotation_ibfk_2` FOREIGN KEY (`annotation`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Image_Relation`
---
-ALTER TABLE `Image_Relation`
-  ADD CONSTRAINT `Image_Relation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Image_Relation_ibfk_2` FOREIGN KEY (`relation`) REFERENCES `Relation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `List_Image`
---
-ALTER TABLE `List_Image`
-  ADD CONSTRAINT `List_Image_ibfk_1` FOREIGN KEY (`list`) REFERENCES `List` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `List_Image_ibfk_2` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Image_ibfk_1` FOREIGN KEY (`list`) REFERENCES `List` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Image_ibfk_2` FOREIGN KEY (`editor`) REFERENCES `Editor` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Contraintes pour la table `Relation`
 --
 ALTER TABLE `Relation`
-  ADD CONSTRAINT `Relation_ibfk_1` FOREIGN KEY (`annotation1`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Relation_ibfk_2` FOREIGN KEY (`annotation2`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Relation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relation_ibfk_2` FOREIGN KEY (`annotation1`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relation_ibfk_3` FOREIGN KEY (`annotation2`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- api/dawagsi_db_structure_if_not_exists.sql
+++ api/dawagsi_db_structure_if_not_exists.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  sam. 01 déc. 2018 à 20:08
+-- Généré le :  sam. 01 déc. 2018 à 20:32
 -- Version du serveur :  5.6.41-log
 -- Version de PHP :  7.2.7
 
@@ -30,9 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `Annotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de l''annotation',
+  `image` int(11) NOT NULL COMMENT 'ID de l''image de l''annotation',
   `tag` varchar(100) NOT NULL COMMENT 'Tag de l''annotation',
   `position` text NOT NULL COMMENT 'Données de position de l''annotation',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `Annotation_ibfk_1` (`image`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,39 +59,11 @@ CREATE TABLE IF NOT EXISTS `Image` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de l''image',
   `originalName` varchar(100) NOT NULL COMMENT 'Nom original de l''image',
   `generatedName` varchar(100) NOT NULL COMMENT 'Nom md5 de l''image',
+  `list` int(11) NOT NULL COMMENT 'ID de la liste de l''image',
   `editor` int(11) DEFAULT NULL COMMENT 'ID de l''éditeur lié à l''image',
   PRIMARY KEY (`id`),
-  KEY `Image_ibfk_1` (`editor`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Image_Annotation`
---
-
-CREATE TABLE IF NOT EXISTS `Image_Annotation` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre Image et Annotation',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image',
-  `annotation` int(11) NOT NULL COMMENT 'ID de l''annotation',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `image` (`image`,`annotation`),
-  KEY `Image_Annotation_ibfk_2` (`annotation`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Image_Relation`
---
-
-CREATE TABLE IF NOT EXISTS `Image_Relation` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre Image et Relation',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image',
-  `relation` int(11) NOT NULL COMMENT 'ID de la relation',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `image` (`image`,`relation`),
-  KEY `Image_Relation_ibfk_2` (`relation`)
+  KEY `Image_ibfk_1` (`list`),
+  KEY `Image_ibfk_2` (`editor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -108,32 +82,19 @@ CREATE TABLE IF NOT EXISTS `List` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `List_Image`
---
-
-CREATE TABLE IF NOT EXISTS `List_Image` (
-  `id` int(11) NOT NULL COMMENT 'ID de la liaison entre List et Image',
-  `list` int(11) NOT NULL COMMENT 'ID de la liste',
-  `image` int(11) NOT NULL COMMENT 'ID de l''image',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `list` (`list`,`image`),
-  KEY `List_Image_ibfk_2` (`image`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `Relation`
 --
 
 CREATE TABLE IF NOT EXISTS `Relation` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la relation',
+  `image` int(11) NOT NULL COMMENT 'ID de l''image de la relation',
   `predicate` varchar(100) NOT NULL COMMENT 'Prédicat de relation',
   `annotation1` int(11) NOT NULL COMMENT 'ID de la première annotation de la relation',
   `annotation2` int(11) NOT NULL COMMENT 'ID de la seconde annotation de la relation',
   PRIMARY KEY (`id`),
-  KEY `Relation_ibfk_1` (`annotation1`),
-  KEY `Relation_ibfk_2` (`annotation2`)
+  KEY `Relation_ibfk_1` (`image`),
+  KEY `Relation_ibfk_2` (`annotation1`),
+  KEY `Relation_ibfk_3` (`annotation2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -141,38 +102,25 @@ CREATE TABLE IF NOT EXISTS `Relation` (
 --
 
 --
+-- Contraintes pour la table `Annotation`
+--
+ALTER TABLE `Annotation`
+  ADD CONSTRAINT `Annotation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Contraintes pour la table `Image`
 --
 ALTER TABLE `Image`
-  ADD CONSTRAINT `Image_ibfk_1` FOREIGN KEY (`editor`) REFERENCES `Editor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Image_Annotation`
---
-ALTER TABLE `Image_Annotation`
-  ADD CONSTRAINT `Image_Annotation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Image_Annotation_ibfk_2` FOREIGN KEY (`annotation`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Image_Relation`
---
-ALTER TABLE `Image_Relation`
-  ADD CONSTRAINT `Image_Relation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Image_Relation_ibfk_2` FOREIGN KEY (`relation`) REFERENCES `Relation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `List_Image`
---
-ALTER TABLE `List_Image`
-  ADD CONSTRAINT `List_Image_ibfk_1` FOREIGN KEY (`list`) REFERENCES `List` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `List_Image_ibfk_2` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Image_ibfk_1` FOREIGN KEY (`list`) REFERENCES `List` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Image_ibfk_2` FOREIGN KEY (`editor`) REFERENCES `Editor` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Contraintes pour la table `Relation`
 --
 ALTER TABLE `Relation`
-  ADD CONSTRAINT `Relation_ibfk_1` FOREIGN KEY (`annotation1`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Relation_ibfk_2` FOREIGN KEY (`annotation2`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Relation_ibfk_1` FOREIGN KEY (`image`) REFERENCES `Image` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relation_ibfk_2` FOREIGN KEY (`annotation1`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relation_ibfk_3` FOREIGN KEY (`annotation2`) REFERENCES `Annotation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
