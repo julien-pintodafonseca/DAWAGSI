35eb6f59f9b1fd64699e9011e62209a2061a4f3e
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-12-29 15:27:28 +0100

    Suppression du code d'exemple HttpClient angular

--- dawagsi/src/app/components/home/home.component.ts
+++ dawagsi/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,43 +7,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  constURL: string = "http://skydefr.com/ptut/api/slim"; //temporaire, à supprimer plus tard
-
   /* Constructeur */
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  ngOnInit() {
-    /* temporaire, à supprimer plus tard */
-    var partialURL = "";
-    var body = "";
-
-    var name = "";
-    var description = "";
-    var images = "";
-
-    partialURL = "/list";
-    //this.http.get<string>(this.constURL + partialURL).subscribe(res => console.log(res));
-
-    partialURL = "/list/selectAll";
-    this.http.get<string>(this.constURL + partialURL).subscribe(res => console.log(res));
-
-    partialURL = "/list/create";
-    name = "NomAngular";
-    description = "DescriptionAngular";
-    //this.http.post(this.constURL + partialURL + '?name=' + name + '&description=' + description, body).subscribe(res => console.log(res));
-
-    partialURL = "/list/1";
-    //this.http.get<string>(this.constURL + partialURL).subscribe(res => console.log(res));
-
-    partialURL = "/list/1";
-    name = "NomAngularModifié";
-    description = "DescriptionAngularModifiée";
-    images = "1,2,3";
-    //this.http.put(this.constURL + partialURL + '?name=' + name + '&description=' + description + '&images=' + images, body).subscribe(res => console.log(res));
-
-    partialURL = "/list/2";
-    //this.http.delete<string>(this.constURL + partialURL).subscribe(res => console.log(res));
-    /* /temporaire, à supprimer plus tard */
-  }
+  ngOnInit() { }
 
 }
